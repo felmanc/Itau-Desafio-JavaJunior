e362bf569fe7d895060bbab6faaee76f8dd05f51
felmanc <felmanc@gmail.com>
2025-03-07 17:07:03 -0300

    Desabilitada configuração para Banco de Dados

--- code/apitransacao/src/main/java/br/com/felmanc/apitransacao/ApitransacaoApplication.java
+++ code/apitransacao/src/main/java/br/com/felmanc/apitransacao/ApitransacaoApplication.java
@@ -2,12 +2,13 @@ package br.com.felmanc.apitransacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApitransacaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApitransacaoApplication.class, args);
 	}
-
 }
