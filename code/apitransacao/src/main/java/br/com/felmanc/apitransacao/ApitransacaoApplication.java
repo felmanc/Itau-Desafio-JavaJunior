f7ae7a651d6375afaceb8dc40e41904665a3a29b
felmanc <felmanc@gmail.com>
2025-03-07 14:03:36 -0300

    Criado projeto Java
    Java
    SpringBoot
    Gradle/Kotlin
    Lombok

---
+++ code/apitransacao/src/main/java/br/com/felmanc/apitransacao/ApitransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.felmanc.apitransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitransacaoApplication.class, args);
+	}
+
+}
---
+++ code/apitransacao/src/test/java/br/com/felmanc/apitransacao/ApitransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felmanc.apitransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
