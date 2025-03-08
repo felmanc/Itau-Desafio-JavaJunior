35b5e7a9f0ce5d95925a7f992235c5bb3169b660
felmanc <felmanc@gmail.com>
2025-03-08 11:24:19 -0300

    Adicionado webmvc-api

--- code/apitransacao/build.gradle.kts
+++ code/apitransacao/build.gradle.kts
@@ -27,10 +27,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.3")
+	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
